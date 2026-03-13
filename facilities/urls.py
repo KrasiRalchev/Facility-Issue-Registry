5754c1dfbd5af2f6732e176bb1a32d175a149f46
Krasimir <krasiair@gmail.com>
2026-03-13 23:23:00 +0200

    correction in facilities/urls and back botton added in facility_detail.html

--- facilities/urls.py
+++ facilities/urls.py
@@ -7,7 +7,7 @@ app_name = 'facilities'
 
 urlpatterns = [
     path('', facility_dashboard, name='facility-dashboard'),
-    path('facilities/', FacilityListView.as_view(), name='facility-list'),
+    path('list/', FacilityListView.as_view(), name='facility-list'),
     path('create/', FacilityCreateView.as_view(), name='facility-create'),
     path('<int:pk>/edit/', FacilityEditView.as_view(), name='facility-edit'),
     path('<int:pk>/delete/', FacilityDeleteView.as_view(), name='facility-delete'),
