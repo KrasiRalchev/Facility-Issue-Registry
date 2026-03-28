91294db9f776e7631d8368a91ffb1211abe6c901
Krasimir <krasiair@gmail.com>
2026-03-28 20:39:27 +0200

    maintenance/view - from fbv to cbv

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from maintenance.views import action_create, resolve_action
-
+from maintenance.views import resolve_action, ActionCreateView
 app_name = "maintenance"
 
 urlpatterns = [
-    path('<int:issue_pk>/create/', action_create, name='action-create'),
+    path('<int:issue_pk>/create/', ActionCreateView.as_view(), name='action-create'),
     path('<int:issue_pk>/resolve/', resolve_action, name='action-resolve'),
 ]
--- maintenance/views.py
+++ maintenance/views.py
@@ -1,30 +1,43 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
 
 from issues.choices import Status_choices
 from issues.models import Issue
 from maintenance.forms import MaintenanceCreateForm, MaintenanceResolveForm
+from maintenance.models import MaintenanceAction
+
+from django.views.generic import CreateView, UpdateView
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
 
 
-def action_create(request: HttpRequest, issue_pk: int) -> HttpResponse:
-    issue = get_object_or_404(Issue, pk=issue_pk)
+class ActionCreateView(LoginRequiredMixin, CreateView):
+    model = MaintenanceAction
+    form_class = MaintenanceCreateForm
+    template_name = 'maintenance/action_create.html'
 
-    form = MaintenanceCreateForm(request.POST or None)
+    def dispatch(self, request, *args, **kwargs):
+        self.issue = get_object_or_404(Issue, pk=self.kwargs['issue_pk'])
+        return super().dispatch(request, *args, **kwargs)
 
-    if request.POST and form.is_valid():
-        action = form.save(commit=False)
-        action.issue = issue
-        action.save()
-        issue.status = Status_choices.IN_PROGRESS
-        issue.save()
+    def form_valid(self, form):
+        form.instance.issue = self.issue
+        response = super().form_valid(form)
 
-        return redirect('issues:issue-detail', issue_pk)
+        self.issue.status = Status_choices.IN_PROGRESS
+        self.issue.save()
 
-    context = {
-        'form': form,
-        'issue': issue,
-    }
-    return render(request, 'maintenance/action_create.html', context)
+        return response
+
+    def get_success_url(self):
+        return reverse('issues:issue-detail', kwargs={'pk': self.issue.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['issue'] = self.issue
+        return context
+
 
 def resolve_action(request: HttpRequest, issue_pk: int) -> HttpResponse:
     issue = get_object_or_404(Issue, pk=issue_pk)
@@ -50,11 +63,6 @@ def resolve_action(request: HttpRequest, issue_pk: int) -> HttpResponse:
     }
 
     return render(request, 'maintenance/action_resolve.html', context)
-
-
-
-
-
 
 
 
