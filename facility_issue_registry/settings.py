91dfa97dda5597d3686c0db8e7c46ab4e8b01e5f
Krasimir <krasiair@gmail.com>
2026-04-02 21:41:38 +0300

    Fix CSRF trusted origins

--- facility_issue_registry/settings.py
+++ facility_issue_registry/settings.py
@@ -32,6 +32,7 @@ DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = [host for host in (os.getenv('ALLOWED_HOSTS').split(',')) if host]
 
+CSRF_TRUSTED_ORIGINS = [host for host in (os.getenv('CSRF_TRUSTED_ORIGINS').split(',')) if host]
 
 # Application definition
 
